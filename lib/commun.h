f05511040065cef373e1926dd1f684cf981dde7c
Hugo Ayaden <hugoayaden@gmail.com>
2026-02-11 19:17:27 +0100

    Carte finalisée et Spawn monstre fait

--- lib/commun.h
+++ lib/commun.h
@@ -1,30 +1,18 @@
-
-typedef enum {FAUX,VRAI} booleen_t;
-
-
-/* 
- * certaines caméras peuvent être initialisées mais ne pas être accessible
- * le monstre s'y trouve mais il n'est pas possible pour le joueur
- * d'en être certain (peur)
+/**
+ * \file carte.h
+ * \brief Header pour la structure de la carte de jeu et du type booleen_t
+ * \author Ayaden Hugo
+ * \version 1.0
+ * \date 11/02/2026
+ * 
  */
 
- typedef struct case_s {
-    //Y'a t-il un monstre sur la caméra
-    booleen_t habite;
+ /* TAILLE DE LA CARTE */
+#define Y 5
+#define X 3
 
-    int num_camera;
-    //le booleen dit si la caméra est utilisée actuellement ou non (FAUX ou VRAI)
-    booleen_t utilise;
-    //majoriterement VRAI sauf pour des zones d'histoire (cabine de notre collegue)
-    booleen_t acess;
+/* POSITION DU JOUEUR */
+#define Y_JOUEUR 4
+#define X_JOUEUR 1
 
-    struct case_s * voisin_haut;
-    struct case_s * voisin_droit;
-    struct case_s * voisin_gauche;
-    struct case_s * voisin_bas;
-
- }case_t;
-
- typedef struct carte_s {
-    case_t cases[4][3];
-}carte_t;
+typedef enum {FAUX,VRAI} booleen_t;
