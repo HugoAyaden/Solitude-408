c257f100d434229578f7f14a026af654184239e0
Bastien-Taugourdeau <bastien.taugourdeau@gmail.com>
2026-03-19 11:26:55 +0100

    credit update

--- src/credit.c
+++ src/credit.c
@@ -3,49 +3,64 @@
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
 
-// ==========================
-// Fonction des crédits
-// ==========================
-void afficher_credits(SDL_Renderer *renderer, SDL_Window *window, TTF_Font *font)
+void afficher_credits(SDL_Renderer *renderer, SDL_Window *window)
 {
+    TTF_Font *fontTitre = TTF_OpenFont("./assets/font/VCR.ttf", 100);
+    TTF_Font *fontSousTitre = TTF_OpenFont("./assets/font/VCR.ttf", 40);
+    TTF_Font *fontTexte = TTF_OpenFont("./assets/font/VCR.ttf", 28);
+    if (!fontTexte || !fontTitre || !fontSousTitre)
+    {
+        printf("Erreur police\n");
+        return ;
+    }
     int width, height;
     SDL_GetWindowSize(window, &width, &height);
 
-    SDL_Surface *bgSurface = IMG_Load("background.png");
-    if (!bgSurface) {
+    SDL_Surface *bgSurface = IMG_Load("./assets/img/background/background.png");
+    if (!bgSurface)
+    {
         printf("Erreur chargement image\n");
         return;
     }
     SDL_Texture *background = SDL_CreateTextureFromSurface(renderer, bgSurface);
     SDL_FreeSurface(bgSurface);
 
-
     const char *credits[] = {
-        " ",
         "Solitude408",
         " ",
+        " ",
+        "Projet de L2 informatique 2025-2026",
+        " ",
+        " ",
         "Developpeur :",
+        " ",
         "Louay AMARA",
         "Hugo AYADEN",
         "Mathis DOS SANTOS",
         "Bastien LEFEVRE TAUGOURDEAU",
         " ",
-        "Graphismes :",
-        "A definir",
+        " ",
+        "Outils utilisés :",
+        " ",
+        "SDL2 (fenêtre du jeu)",
+        "SDL2_image",
+        "SDL2_ttf",
+        "Compilateur GCC",
+        "Langage C",
+        "Gimp (images)",
+        "Audacity (sons)",
+        " ",
         " ",
         "Musique :",
         "A definir",
         " ",
         "Merci d'avoir joue !",
-        NULL
-    };
+        NULL};
 
-    SDL_Color color = {255, 255, 0, 255}; // jaune style Star Wars
+    SDL_Color color = {147, 112, 219, 255}; //purple pour respecter la DA 
 
-    int y = WINDOW_HEIGHT; // départ en bas
+    int y = height;
     int quit = 0;
     SDL_Event event;
 
@@ -64,13 +79,24 @@ void afficher_credits(SDL_Renderer *renderer, SDL_Window *window, TTF_Font *font
 
         for (int i = 0; credits[i] != NULL; i++)
         {
-            SDL_Surface *surface = TTF_RenderText_Solid(font, credits[i], color);
+            TTF_Font *currentFont = fontTexte;
+
+            if (strcmp(credits[i], "Solitude408") == 0)
+                currentFont = fontTitre;
+            if (strcmp(credits[i], "Developpeur :") == 0)
+                currentFont = fontSousTitre;
+            if (strcmp(credits[i],  "Outils utilisés :") == 0)
+                currentFont = fontSousTitre;
+            if (strcmp(credits[i],  "Projet de L2 informatique 2025-2026") == 0)
+                currentFont = fontSousTitre;
+
+            SDL_Surface *surface = TTF_RenderText_Solid(currentFont, credits[i], color);
             SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
 
             SDL_Rect rect;
             rect.w = surface->w;
             rect.h = surface->h;
-            rect.x = (WINDOW_WIDTH - rect.w) / 2;
+            rect.x = (width - rect.w) / 2;
             rect.y = currentY;
 
             SDL_RenderCopy(renderer, texture, NULL, &rect);
@@ -89,19 +115,18 @@ void afficher_credits(SDL_Renderer *renderer, SDL_Window *window, TTF_Font *font
             quit = 1;
 
         SDL_Delay(16); // ~60 FPS
-        SDL_DestroyTexture(background);
     }
+    SDL_DestroyTexture(background);
+    TTF_CloseFont(fontTexte);
+    TTF_CloseFont(fontSousTitre);
+    TTF_CloseFont(fontTitre);
 }
-
 
 // ==========================
 // MAIN DE TEST
 // ==========================
-int main(int argc, char *argv[])
+int main()
 {
-    (void)argc;
-    (void)argv;
-
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
         return 1;
 
@@ -113,22 +138,15 @@ int main(int argc, char *argv[])
     SDL_Window *window = SDL_CreateWindow("Credits",
                                           SDL_WINDOWPOS_CENTERED,
                                           SDL_WINDOWPOS_CENTERED,
-                                          800,
-                                          600,
-                                          0);
+                                          0,
+                                          0,
+                                          SDL_WINDOW_FULLSCREEN_DESKTOP);
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    TTF_Font *font = TTF_OpenFont("./assets/font/VCR.ttf", 28);
-    if (!font)
-    {
-        printf("Erreur police\n");
-        return 1;
-    }
 
-    afficher_credits(renderer, window, font);
+    afficher_credits(renderer, window); // la fonction à appeller 
 
-    TTF_CloseFont(font);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
 
