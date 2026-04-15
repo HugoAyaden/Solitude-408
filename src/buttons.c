59221f62fbb1a21a0048bd1591b9b776cdf7bdee
Hugo <hugoayaden@gmail.com>
2026-04-15 09:06:13 +0200

    simplified cameraon variables

--- src/buttons.c
+++ src/buttons.c
@@ -9,6 +9,7 @@
 
 #include "buttons.h"
 
+
 /**
  * \brief Initializes the state of all buttons.
  *
@@ -31,6 +32,7 @@ void buttons_init()
     camera7on = 0;
     camera8on = 0;
     camera9on = 0;
+    int cameraTab[] = {camera1on, camera2on, camera3on, camera4on, camera5on, camera6on, camera7on, camera8on, camera9on};
 }
 
 /**
